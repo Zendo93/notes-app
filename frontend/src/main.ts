8ca87141a85c40244ac0ad3b8cf62aac4fd3f9a0
Richard Uzsak <Richard.Uzsak@aomvsr.sk>
2026-02-24 13:00:02 +0100

    vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,39 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Fonts for Vuetify
+import '@fontsource/roboto/100.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/roboto/900.css'
+
+/* optional italic styles */
+import '@fontsource/roboto/100-italic.css'
+import '@fontsource/roboto/300-italic.css'
+import '@fontsource/roboto/400-italic.css'
+import '@fontsource/roboto/500-italic.css'
+import '@fontsource/roboto/700-italic.css'
+import '@fontsource/roboto/900-italic.css'
 
 import App from './App.vue'
 import router from './router'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
