3ea3ce69063e4f94f1e327cbfcb905bd3b6909cd
Richard Uzsak <Richard.Uzsak@aomvsr.sk>
2026-05-10 23:28:53 +0200

    style select, search

--- frontend/src/components/layout/Filters.vue
+++ frontend/src/components/layout/Filters.vue
@@ -1,8 +1,8 @@
 <template>
     <v-sheet class="pa-4" color="surface-variant">
-        <app-search-input v-model="search" class="w-50 mb-5" density="compact" prepend-inner-icon="mdi-magnify" clear-icon="mdi-close-circle-outline" label="Search Company Directory" variant="solo" />
-        <app-select v-model="sort" class="w-25 mb-5" label="Sort" :items="sorts" variant="outlined" />
-        <app-select v-model="category" class="w-25" label="Category" :items="categories as unknown as any[]" variant="outlined" />
+        <app-search-input v-model="search" class="small-search mb-5" density="compact" prepend-inner-icon="mdi-magnify" clear-icon="mdi-close-circle-outline" label="Search Note" variant="solo" />
+        <app-select v-model="sort" class="small-select mb-5" density="compact" label="Sort" :items="sorts" variant="outlined" />
+        <app-select v-model="category" class="small-select" density="compact" label="Category" :items="categories as unknown as any[]" variant="outlined" />
     </v-sheet>
 </template>
 <script setup lang="ts">
@@ -37,3 +37,12 @@ watch([sort, category], () => {
   })
 })
 </script>
+<style scoped>
+.small-select {
+  max-width: 140px;
+}
+
+.small-search {
+  max-width: 240px;
+}
+</style>
